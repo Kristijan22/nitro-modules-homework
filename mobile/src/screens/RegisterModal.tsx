de65b842e060d2e64f3b01e491e881557a708789
Kristijan Vidovic <kristijan.vidovic@getyourguide.com>
2026-06-07 16:18:56 +0200

    Set up notification prompt

--- mobile/src/screens/RegisterModal.tsx
+++ mobile/src/screens/RegisterModal.tsx
@@ -1,16 +1,10 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useFormik } from 'formik';
 import React from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Pressable,
-  ActivityIndicator,
-} from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
+import CustomButton from '../../specs/CustomButtonNativeComponent';
 import { colors } from '../colors';
 import { useLotteryRegister } from '../hooks/useLotteryRegister';
 import { useRegisteredStore } from '../store/useRegisteredStore.ts';
@@ -46,10 +40,6 @@ export const RegisterModal = () => {
     },
   });
 
-  const backgroundColor = !formik.isValid
-    ? colors.grey
-    : colors.buttonSecondary;
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Register to lotteries</Text>
@@ -64,18 +54,12 @@ export const RegisterModal = () => {
       {formik.touched.name && formik.errors.name ? (
         <Text style={styles.error}>{formik.errors.name}</Text>
       ) : null}
-      <Pressable
-        accessibilityRole="button"
-        style={[styles.button, { backgroundColor }]}
-        onPress={() => formik.handleSubmit()}
-        disabled={!formik.isValid}
-      >
-        {isPending ? (
-          <ActivityIndicator />
-        ) : (
-          <Text style={styles.buttonText}>Register</Text>
-        )}
-      </Pressable>
+      <CustomButton
+        text="Register"
+        disabled={!formik.isValid || isPending}
+        onCustomButtonPress={() => formik.handleSubmit()}
+        style={{ width: 120, height: 44, marginTop: 32 }}
+      />
       {error ? <Text style={styles.error}>error</Text> : null}
     </View>
   );
@@ -99,18 +83,6 @@ const styles = StyleSheet.create({
     borderBottomColor: colors.grey,
     fontSize: 16,
     width: 300,
-  },
-  button: {
-    marginTop: 32,
-    width: 120,
-    borderRadius: 4,
-    paddingVertical: 12,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: colors.secondary,
-    fontSize: 16,
-    fontWeight: '600',
   },
   error: {
     fontSize: 10,
